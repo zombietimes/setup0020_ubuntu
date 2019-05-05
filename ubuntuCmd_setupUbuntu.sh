ce8f6011e2574419f0522ca4cd6f74d852910841
royal <royal@yakinkuclub.com>
2019-05-05 19:06:24 +0900

    2019/05/05

--- ubuntuCmd_setupUbuntu.sh
+++ ubuntuCmd_setupUbuntu.sh
@@ -17,6 +17,33 @@ sudo apt-get install python -y
 sudo apt-get install git -y
 
 echo "\n<Setup>"
+echo "Add 'New Document' in the context menu."
+echo "\n"
+touch ~/Templates/Empty\ Document
+
+echo "\n<Setup>"
+echo "Set up Open SSH."
+echo "- If you use virtualBox + Ubuntu,"
+echo "- Open SSH allows accessing from the host OS to the guest OS."
+echo "\n"
+sudo apt-get install openssh-server -y
+sudo service ssh start
+ps aux | grep ssh
+
+echo "\n<Setup>"
+echo "Set up Google Chrome."
+echo "- Go to the official site and download the installer."
+echo "- [URL] https://www.google.co.jp/chrome/browser/desktop/"
+echo ""
+echo "After completed, [Enter] to continue."
+echo "\n"
+read Wait;
+sudo apt-get install libappindicator1 -y
+cd ~/Downloads/
+ls
+sudo dpkg -i ./google-chrome*.deb
+
+echo "\n<Setup>"
 echo "Reboot Ubuntu."
 echo ""
 echo "[Enter] to reboot."
